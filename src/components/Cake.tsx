8ffb32ca76de73f1a82d18844ca87135f7726587
Momo Miller <minimomo14z@hotmail.com>
2022-01-15 12:01:07 -0500

    last updates

--- src/components/Cake.tsx
+++ src/components/Cake.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Layer from "../model/LayerInterface";
+import "./Cake.css"
+
 
 /// set array of layers, displays a cake
 
@@ -11,27 +13,76 @@ function Cake() {
   ]);
   return (
     <div className="Cake">
-      <h1>Build your own Cake!</h1>
+        <div className="Cake_header">
+          <h1>Build your own Cake!</h1>
       <p>
         Cupcake ipsum dolor sit amet croissant caramels macaroon gingerbread.
         Shortbread candy canes pastry candy pastry tootsie roll. Pudding topping
         tootsie roll danish pie gingerbread chocolate cake cake marzipan.
         Fruitcake cake cake candy cotton candy muffin toffee jelly-o.
-      </p>
+      </p>  
+        </div>
+      
       <div className="Cake_addLayer">
         <h2>Add a Layer</h2>
-        <div className="slidecontainer">
+        <div className="Cake_slidecontainer">
           <label>Color:</label>
           <select typeof="color"></select>
-          
+
           <label>Height:</label>
           <input type="range" min="0.5" max="10"></input>
 
           <label>Width:</label>
           <input type="range" min="1" max="10"></input>
-        
-          <button type="submit" value="save">Save</button>
-          <button type="submit" value="cancle">Cancel</button>
+
+            <div></div>
+          <button type="submit" value="save">
+            Save
+          </button>
+           <div></div>
+          <button type="submit" value="cancle">
+            Cancel
+          </button>
+        </div>
+
+        <div className="Cake_slidecontainer">
+          <label>Color:</label>
+          <select typeof="color"></select>
+
+          <label>Height:</label>
+          <input type="range" min="0.5" max="10"></input>
+
+          <label>Width:</label>
+          <input type="range" min="1" max="10"></input>
+
+            <div></div>
+          <button type="submit" value="save">
+            Save
+          </button>
+          <div></div>
+          <button type="submit" value="cancle">
+            Cancel
+          </button>
+        </div>
+
+        <div className="Cake_slidecontainer">
+          <label>Color:</label>
+          <select typeof="color"></select>
+
+          <label>Height:</label>
+          <input type="range" min="0.5" max="10"></input>
+
+          <label>Width:</label>
+          <input type="range" min="1" max="10"></input>
+
+           <div></div>
+          <button type="submit" value="save">
+            Save
+          </button>
+          <div></div>
+          <button type="submit" value="cancle">
+            Cancel
+          </button>
         </div>
       </div>
     </div>
